bae9e5f5f3a008e86e5d5c6a38cb1db179063920
Cecile Hannay <hannay@ucar.edu>
2021-12-15 18:04:12 -0700

    case files for f.e21.FWscHIST.ne30_L48_BL10_cam6_3_035.tphysac_reorder_zm2_trapT.001.hf2

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # amwg_cases
 
 # case: 
-f.e21.FWscHIST.ne30_L48_BL10_cam6_3_035.tphysac_reorder_zm2_tau7200.001.hf
+f.e21.FWscHIST.ne30_L48_BL10_cam6_3_035.tphysac_reorder_zm2_trapT.001.hf2
+
 
 # sandbox:  
 https://github.com/NCAR/amwg_dev/tree/cam6_3_035.tphysac/
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2021-12-08 15:30:17
+# Created 2021-12-15 17:46:26
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.e21.FWscHIST.ne30_L48_BL10_cam6_3_035.tphysac_reorder_zm2_tau7200.001.hf"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.e21.FWscHIST.ne30_L48_BL10_cam6_3_035.tphysac_reorder_zm2_trapT.001.hf2"
 
 /glade/work/hannay/cesm_tags/cam6_3_035.tphysac/cime/scripts/create_newcase --compset HIST_CAM60%WCSC_CLM50%BGC-CROP_CICE%PRES_DOCN%DOM_MOSART_CISM2%NOEVOLVE_SWAV_SIAC_SESP --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -32,7 +32,9 @@ cd "${CASEDIR}"
 
 ./xmlchange GET_REFCASE=TRUE
 
-./preview_namelists
+./case.build
+
+./case.build
 
 ./case.build
 
