e586e9bc98eb73ad7bca28bcfdcfaf7c275df8e3
Cecile Hannay <hannay@ucar.edu>
2022-06-29 16:28:27 -0600

    case files for b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.cice5.cam6.005

--- replay.sh
+++ replay.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-# Created 2022-05-17 17:13:50
+# Created 2022-05-23 09:24:19
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.cam6.005"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.cice5.cam6.005"
 
-/glade/work/hannay/cesm_tags/cesm3_cam6_3_041_MOM3/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cesm3_cam6_3_041_MOM3/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
-./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 58
+./xmlchange CAM_CONFIG_OPTS=-phys cam6 -microphys mg2 -chem waccm_sc_mam4 -nlev 58
 
 ./xmlchange ATM_GRID=ne30np4.pg3
 
@@ -18,10 +18,6 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange CAM_CONFIG_OPTS=-phys cam6 -microphys mg2 -chem waccm_sc_mam4 -nlev 58
-
-./case.build --clean-all
-
 ./case.build
 
 ./case.build
@@ -30,35 +26,55 @@ cd "${CASEDIR}"
 
 ./case.build
 
+./case.submit
+
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=1,STOP_N=12,STOP_OPTION=nmonths
+
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=1,STOP_N=12,STOP_OPTION=nmonths
+
+./case.submit
+
+./case.build
+
 ./case.build
 
 ./case.build
 
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
+
+./case.build
+
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
 
 ./case.submit
 
 ./preview_namelists
 
+./preview_namelists
+
 ./case.submit
+
+./xmlchange RESUBMIT=4
+
+./xmlchange STOP_N=1,RESUBMIT=13
+
+./xmlchange CREATE_ESMF_PET_FILES=TRUE
 
 ./xmlchange JOB_QUEUE=premium
 
+./xmlchange JOB_QUEUE=premium,STOP_N=1
+
 ./case.submit
 
-./preview_namelists
+./xmlchange JOB_QUEUE=regular
+
+./case.submit
+
+./case.setup --reset
+
+./case.build --clean
 
 ./case.build
-
-./case.submit
-
-./case.submit
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
 
 ./case.submit
 
