3d0251e50bd95d19dc25beccf5dba4b4ee500762
Cecile Hannay <hannay@ucar.edu>
2022-06-29 16:23:39 -0600

    case files for b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.cam6.005

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2022-05-04 10:23:57
+# Created 2022-05-17 17:13:50
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.005"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam041_mom.B1850WcMOM.ne30_L58_t061.cam6.005"
 
 /glade/work/hannay/cesm_tags/cesm3_cam6_3_041_MOM3/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,31 +18,47 @@ cd "${CASEDIR}"
 
 ./case.build
 
+./xmlchange CAM_CONFIG_OPTS=-phys cam6 -microphys mg2 -chem waccm_sc_mam4 -nlev 58
+
+./case.build --clean-all
+
+./case.build
+
+./case.build
+
+./case.build
+
+./case.build
+
+./case.build
+
+./case.build
+
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+
+./case.submit
+
+./preview_namelists
+
+./case.submit
+
+./xmlchange JOB_QUEUE=premium
+
+./case.submit
+
 ./preview_namelists
 
 ./case.build
 
 ./case.submit
 
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=1,STOP_OPTION=nyears
-
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=1,STOP_OPTION=nyears
-
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
-
 ./case.submit
 
 ./preview_namelists
 
-./case.submit
+./preview_namelists
 
-./xmlchange RESUBMIT=3
-
-./case.submit
-
-./xmlchange RESUBMIT=20
+./preview_namelists
 
 ./case.submit
-
-./xmlchange RESUBMIT=0
 
