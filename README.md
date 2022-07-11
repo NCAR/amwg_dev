9bd9af5c339af177637232f5e968a4df7023640c
Cecile Hannay <hannay@ucar.edu>
2022-07-11 11:59:47 -0600

    case files for b.cesm3_cam058_mom_c.B1850WscMOM.f09_L58_t061.011

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/137
+https://github.com/NCAR/amwg_dev/issues/138
--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2022-07-08 13:45:22
+# Created 2022-07-09 21:52:42
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_c.B1850WscMOM.ne30_L58_t061.010"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_c.B1850WscMOM.f09_L58_t061.011"
 
-/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_c/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case b.cesm3_cam058_mom_c.B1850WscMOM.ne30_L58_t061.010 --run-unsupported
+/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_c/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res f09_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
@@ -14,49 +14,17 @@ cd "${CASEDIR}"
 
 ./case.setup
 
-./xmlchange JOB_QUEUE=regular,STOP_N=1,STOP_OPTION=ndays
-
-./xmlchange JOB_QUEUE=premium
-
-./xmlchange RUN_TYPE=hybrid
+./case.build
 
 ./case.build
 
-./xmlchange RUN_REFCASE=gmom.e23.GJRAv3.TL319_t061_zstar_N75.nuopc.cice6salt.001
+./case.build
 
-./xmlchange RUN_REFDATE=0060-01-01
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
 
-./xmlchange DOUT_S=False
-
-./check_case
+./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.submit
 
-./xmlchange JOB_WALLCLOCK_TIME=01:00:00
-
-./case.submit
-
-./xmlchange RUN_TYPE=initial
-
-./xmlchange RUN_TYPE=startup
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./xmlchange STOP_OPTION=nyears
-
-./xmlchange DOUT_S=True
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00
-
-./xmlchange JOB_QUEUE=regular
-
-./xmlchange STOP_N=3
-
-./xmlchange RESUBMIT=9
-
-./case.submit
+./xmlchange RESUBMIT=4
 
