58eec674e5b7c64efc996ab32316394f51a41b54
Cecile Hannay <hannay@ucar.edu>
2022-07-22 17:43:17 -0600

    case files for b.cesm3_cam058_mom_e.B1850MOM.f09_L32_t061.cam6_cice5.014

--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2022-07-20 16:50:51
+# Created 2022-07-21 16:16:22
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_d.B1850MOM.f09_L32_t061.cam6_cice5.013"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850MOM.f09_L32_t061.cam6_cice5.014"
 
-/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_d/cime/scripts/create_newcase --compset 1850_CAM60_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res f09_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res f09_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
@@ -20,8 +20,6 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./case.build
-
 ./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
@@ -32,27 +30,17 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
-./preview_namelists
-
-./xmlchange JOB_QUEUE=premium,STOP_N=1
-
-./case.submit
-
 ./xmlchange CAM_CONFIG_OPTS=-phys cam6
 
 ./case.build --clean-all
 
 ./case.build
 
-./case.submit
+./xmlchange JOB_QUEUE=premium,STOP_N=1
 
 ./case.submit
 
-./case.submit
+./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=3,STOP_OPTION=nyears
 
-./xmlchange JOB_QUEUE=premium
-
-./xmlchange RESUBMIT=0
+./xmlchange REST_OPTION=nyears,REST_N=1
 
