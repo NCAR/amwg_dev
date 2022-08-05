49042d50fbbc20bf26df82641b76a4a668558b22
Cecile Hannay <hannay@ucar.edu>
2022-08-05 14:48:48 -0600

    case directory for b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.cam6_cice5.018b

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2022-07-29 16:48:47
+# Created 2022-07-30 10:30:29
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.cam6_cice5.018"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850MOM.ne30_L32_t061.cam6_cice5.018b"
 
 /glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,13 +18,13 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./case.build
-
 ./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=3,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.submit
 
-./xmlchange RESUBMIT=0
+./xmlchange RESUBMIT=30
+
+./xmlchange RESUBMIT=1
 
