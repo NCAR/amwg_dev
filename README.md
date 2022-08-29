3d440e9d4caccaef1b4cbc75ff3360596e7673c6
Cecile Hannay <hannay@ucar.edu>
2022-08-29 16:28:33 -0600

    case files for b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L32_t061.camdev_cice5.022d

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/167
+https://github.com/NCAR/amwg_dev/issues/168
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2022-08-23 16:41:31
+# Created 2022-08-23 16:43:04
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L32_t061.camdev_cice5.022c"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L32_t061.camdev_cice5.022d"
 
 /glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -26,17 +26,7 @@ cd "${CASEDIR}"
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
-./case.build
-
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=3,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
 ./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=5
 
 ./xmlchange RESUBMIT=5
 
