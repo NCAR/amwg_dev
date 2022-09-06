9323850c6076318cd8e9b563a0fd53a2ad00f696
Cecile Hannay <hannay@ucar.edu>
2022-09-06 15:28:54 -0600

    case files for b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.025a

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2022-08-23 16:41:31
+# Created 2022-09-01 10:25:11
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L32_t061.camdev_cice5.022c"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.025a"
 
 /glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -14,11 +14,7 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 32
-
-./case.build
-
-./case.build
+./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 58
 
 ./case.build
 
@@ -33,12 +29,6 @@ cd "${CASEDIR}"
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=5
-
-./xmlchange RESUBMIT=5
 
 ./case.submit
 
