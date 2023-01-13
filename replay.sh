547d706500375aa0483cc88b8162676285ea7e20
Cecile Hannay <hannay@ucar.edu>
2023-01-13 15:12:22 -0700

    case files for f.cesm3_cam058_mom_e.FWscHIST.ne30_L58.26c_topofix.001

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2022-10-24 17:45:19
+# Created 2023-01-06 16:55:39
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cesm3_cam058_mom_e.FWscHIST.ne30_L58.26c_non-orogw_on.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cesm3_cam058_mom_e.FWscHIST.ne30_L58.26c_topofix.001"
 
 /glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset FWscHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -13,6 +13,40 @@ cd "${CASEDIR}"
 ./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 58
 
 ./case.setup
+
+./xmlchange RUN_STARTDATE=1979-01-01
+
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.e21.FWscHIST_BGC.ne30_ne30_mg17_L48_revert-J.001
+
+./xmlchange RUN_REFDATE=1989-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./case.build
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./preview_namelists
+
+./case.build
+
+./case.build
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
+
+./case.build --clean-all
 
 ./xmlchange RUN_STARTDATE=1979-01-01
 
