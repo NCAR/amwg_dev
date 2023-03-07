e0103897780e4a50ba5179a0efcbf0230607cb06
Cecile Hannay <hannay@ucar.edu>
2023-03-07 15:04:42 -0700

    case files for b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.026g

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/194
+https://github.com/NCAR/amwg_dev/issues/195
 
--- replay.sh
+++ replay.sh
@@ -2,31 +2,81 @@
 
 set -e
 
-# Created 2022-10-25 15:31:15
+# Created 2022-10-25 19:49:02
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.026f"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.026g"
 
-/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cesm3_cam6_3_058_MOM_e/cime/scripts/create_newcase --compset 1850_CAM60%WCSC_CLM50%BGC-CROP_CICE5_MOM6_MOSART_CISM2%GRIS-NOEVOLVE_SWAV_SESP_BGC%BDRD --res ne30pg3_t061 --case b.cesm3_cam058_mom_e.B1850WscMOM.ne30_L58_t061.camdev_cice5.026g --run-unsupported --project CESM0002
 
 cd "${CASEDIR}"
 
+./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 58
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_QUEUE=premium
+
+./case.build
+
 ./case.setup
 
-./preview_namelists
+./case.build
+
+./case.setup --reset
+
+./case.build
 
 ./xmlchange CAM_CONFIG_OPTS=-phys cam_dev -microphys mg2 -chem waccm_sc_mam4 -nlev 58
 
 ./case.build
 
+./case.setup --reset
+
 ./case.build
 
-./xmlchange PROJECT=CESM0019,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=3,STOP_OPTION=nyears
+./case.build --clean-all
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./case.build
 
 ./case.submit
 
-./xmlchange JOB_QUEUE=premium
+./case.submit
+
+./xmlchange JOB_QUEUE=premium,RESUBMIT=10,STOP_N=3,STOP_OPTION=nyears
+
+./case.submit
+
+./case.submit
+
+./xmlchange RESUBMIT=11
+
+./case.submit
+
+./xmlchange JOB_QUEUE=regular
+
+./xmlchange RESUBMIT=9
+
+./case.submit
+
+./case.submit
+
+./xmlchange PROJECT=cesm0023
+
+./xmlchange RESUBMIT=12
+
+./case.submit
+
+./xmlchange RESUBMIT=10
+
+./case.submit
+
+./xmlchange RESUBMIT=12
+
+./case.submit
+
+./xmlchange RESUBMIT=9
+
+./case.submit
 
 ./case.submit
 
