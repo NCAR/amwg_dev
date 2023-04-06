571a41899bf370d6838a26be5793635c16ed2d16
Cecile Hannay <hannay@ucar.edu>
2023-04-06 17:03:12 -0600

    case files for f.cam6_3_102.FLTHIST_v0a.ne30.001

--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2023-04-03 13:44:32
+# Created 2023-04-04 14:32:45
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_101.FLTHIST_v0a.ne30.003"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_102.FLTHIST_v0a.ne30.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_101/cime/scripts/create_newcase --compset FLTHIST_v0a --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_102/cime/scripts/create_newcase --compset FLTHIST_v0a --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
@@ -34,21 +34,71 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./case.build
+./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
 
-./case.build
+./xmlchange REST_OPTION=nyears,REST_N=1
 
-./case.build
+./case.submit
 
-./case.build --clean-all
+./preview_namelists
 
-./case.build
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./preview_namelists
+
+./case.submit
+
+./preview_namelists
 
 ./case.build
 
 ./case.build
 
 ./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./case.submit
+
+./case.build --clean-all
+
+./case.build
+
+./case.submit
+
+./preview_namelists
+
+./preview_namelists
+
+./case.submit
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=3,STOP_N=3,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./case.submit
+
+./case.build
+
+./case.build
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./case.submit
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=1,STOP_N=1,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
