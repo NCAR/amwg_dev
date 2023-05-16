239fd1d085fe6a457f001d49caf4a65be5d9cbf3
Cecile Hannay <hannay@ucar.edu>
2023-05-16 13:37:33 -0600

    case files for f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1_ebudget_tuning1.001

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-04-20 14:04:12
+# Created 2023-04-24 16:47:39
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1_ebudget.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1_ebudget_tuning1.001"
 
 /glade/work/hannay/cesm_tags/cam6_3_107_ebudget_dev_update/cime/scripts/create_newcase --compset HIST_CAM%DEV%LT%GHGMAM4_CLM51%SP_CICE%PRES_DOCN%DOM_MOSART_SGLC_SWAV --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -34,41 +34,85 @@ cd "${CASEDIR}"
 
 ./case.build
 
+./xmlchange RUN_STARTDATE=1995-01-01
+
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1_ebudget.001
+
+./xmlchange RUN_REFDATE=1995-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./case.build
+
+./xmlchange RUN_STARTDATE=1995-01-01
+
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+
+./xmlchange RUN_REFDATE=1995-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./xmlchange RUN_STARTDATE=1995-01-01
+
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+
+./xmlchange RUN_REFDATE=1986-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
 ./preview_namelists
 
 ./case.build
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+./case.build
+
+./case.build --clean-all
+
+./case.build
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.submit
 
-./preview_namelists
-
-./preview_namelists
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=1,STOP_N=1,STOP_OPTION=ndays
-
-./xmlchange REST_OPTION=nyears,REST_N=1
+./xmlchange JOB_QUEUE=premium
 
 ./case.submit
 
-./case.submit
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=1,STOP_N=1,STOP_OPTION=ndays
-
-./xmlchange REST_OPTION=ndays,REST_N=1
-
-./case.submit
-
-./xmlchange CONTINUE_RUN=FALSE
-
-./case.submit
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=15,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./case.submit
+./xmlchange JOB_QUEUE=regular
 
