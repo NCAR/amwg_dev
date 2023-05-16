c91291880b4c17df2bc1b9672234994c2f223fa1
Cecile Hannay <hannay@ucar.edu>
2023-05-16 13:52:48 -0600

    case files for f.cam6_3_109.FLTHIST_v0b.ne30.tuningF_non-ogw.001

--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2023-05-10 15:15:14
+# Created 2023-05-15 16:17:20
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_110.FMTHIST_v0c.ne30.tuningF.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_109.FLTHIST_v0b.ne30.tuningF_non-ogw.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_110/cime/scripts/create_newcase --compset FMTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_109/cime/scripts/create_newcase --compset FLTHIST_v0b --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
@@ -32,25 +32,17 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
-./case.build
+./preview_namelists
 
 ./case.build
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+./preview_namelists
+
+./case.build
+
+./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
-
-./case.submit
-
-./case.submit
-
-./xmlchange STOP_N=nyears,RESUBMIT=10
-
-./xmlchange STOP_N=1,STOP_OPTION=nyears,RESUBMIT=10
-
-./case.submit
-
-./xmlchange STOP_N=1,STOP_OPTION=nyears,RESUBMIT=10,JOB_QUEUE=regular
 
 ./case.submit
 
