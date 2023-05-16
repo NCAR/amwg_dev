47673782cd39ddddce2a916965fb1a226d9f1d80
Cecile Hannay <hannay@ucar.edu>
2023-05-16 13:45:46 -0600

    case files for f.cam6_3_110.FMTHIST_v0c.ne30.tuningF.001

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/259
+Issue https://github.com/NCAR/amwg_dev/issues/263
 
 
--- replay.sh
+++ replay.sh
@@ -2,37 +2,13 @@
 
 set -e
 
-# Created 2023-05-05 15:19:52
+# Created 2023-05-10 15:15:14
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_109.FLTHIST_v0b.ne30.tuningF.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_110.FMTHIST_v0c.ne30.tuningF.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_109/cime/scripts/create_newcase --compset FLTHIST_v0b --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_110/cime/scripts/create_newcase --compset FMTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
-
-./xmlchange --append CAM_CONFIG_OPTS=-cosp
-
-./case.setup
-
-./xmlchange RUN_STARTDATE=1995-01-01
-
-./xmlchange STOP_N=2
-
-./xmlchange STOP_OPTION=nyears
-
-./xmlchange RESUBMIT=5
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
-
-./xmlchange RUN_REFDATE=1995-01-01
-
-./xmlchange GET_REFCASE=TRUE
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
 
 ./xmlchange --append CAM_CONFIG_OPTS=-cosp
 
@@ -56,11 +32,7 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
+./case.build
 
 ./case.build
 
@@ -70,41 +42,15 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./case.build
+./case.submit
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
+./xmlchange STOP_N=nyears,RESUBMIT=10
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./xmlchange STOP_N=1,STOP_OPTION=nyears,RESUBMIT=10
 
 ./case.submit
 
-./preview_namelists
-
-./case.submit
-
-./xmlchange STOP_N=2,STOP_OPTION=nyears,JOB_QUEUE=regular
-
-./xmlchange STOP_N=2,STOP_OPTION=nyears,JOB_QUEUE=regular,RESUBMIT=5
-
-./preview_namelists
-
-./case.submit
-
-./xmlchange STOP_N=1,STOP_OPTION-nmonths
-
-./xmlchange STOP_N=1,STOP_OPTION=nmonths
-
-./case.submit
-
-./xmlchange RESUBMIT=10
-
-./xmlchange CONTINUE_RUN=FALSE
-
-./case.submit
-
-./xmlchange STOP_OPTION=nyears
-
-./xmlchange CONTINUE_RUN=FALSE
+./xmlchange STOP_N=1,STOP_OPTION=nyears,RESUBMIT=10,JOB_QUEUE=regular
 
 ./case.submit
 
