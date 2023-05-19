f1041c9cb679bbfd83ecf8e9d96a085ba176d0f0
Cecile Hannay <hannay@ucar.edu>
2023-05-19 09:25:18 -0600

    case files for f.cam6_3_109.FLTHIST_v0b.ne30.tuningF_non-ogw_ubcF.001

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-05-15 16:32:06
+# Created 2023-05-16 11:48:58
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_109.FLTHIST_v0b.ne30.tuningF_hb.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_109.FLTHIST_v0b.ne30.tuningF_non-ogw_ubcF.001"
 
 /glade/work/hannay/cesm_tags/cam6_3_109/cime/scripts/create_newcase --compset FLTHIST_v0b --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -33,16 +33,6 @@ cd "${CASEDIR}"
 ./xmlchange RUN_REFDIR=cesm2_init
 
 ./case.build
-
-./case.build
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.build
 
