55e920eeb23353f932e03ef56b59a7836b7c8e1b
Cecile Hannay <hannay@ucar.edu>
2023-06-20 11:43:29 -0600

    case files for f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-gamma.001

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/296
+Issue https://github.com/NCAR/amwg_dev/issues/297
 
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-06-05 09:46:20
+# Created 2023-06-08 15:33:54
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-gamma.001"
 
 /glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset FLTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -31,6 +31,32 @@ cd "${CASEDIR}"
 ./xmlchange GET_REFCASE=TRUE
 
 ./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./xmlchange --append CAM_CONFIG_OPTS=-cosp
+
+./case.setup
+
+./xmlchange RUN_STARTDATE=1995-01-01
+
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+
+./xmlchange RUN_REFDATE=1994-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./case.build
 
 ./case.build
 
