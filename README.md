1d6894e4603d567ea9b3e4c5496b950e8b51a754
Cecile Hannay <hannay@ucar.edu>
2023-06-29 20:44:22 -0600

    case files for f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi2.0.001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/312
+Issue https://github.com/NCAR/amwg_dev/issues/313
 
 
 
--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2023-06-23 17:36:11
+# Created 2023-06-26 15:50:36
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FMTHIST_v0c.ne30.non-ogw-ubcT-effgw0.7_taubgnd2.5.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi2.0.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset FMTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset FLTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
@@ -24,27 +24,15 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_TYPE=hybrid
 
-./xmlchange RUN_REFCASE=f.cam6_3_110.FMTHIST_v0c.ne30.tuningF.001
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
 
-./xmlchange RUN_REFDATE=1996-01-01
+./xmlchange RUN_REFDATE=1994-01-01
 
 ./xmlchange GET_REFCASE=TRUE
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
 ./case.build
-
-./preview_namelists
-
-./case.build
-
-./case.build
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=premium,RESUBMIT=0,STOP_N=1,STOP_OPTION=nmonths
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./case.submit
 
 ./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
 
@@ -54,11 +42,25 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./case.build --clean-all
+./xmlchange --append CAM_CONFIG_OPTS=-cosp
 
-./case.build
+./case.setup
 
-./case.submit
+./xmlchange RUN_STARTDATE=1995-01-01
 
-./xmlchange RESUBMIT=0
+./xmlchange STOP_N=2
+
+./xmlchange STOP_OPTION=nyears
+
+./xmlchange RESUBMIT=5
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+
+./xmlchange RUN_REFDATE=1994-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
 
