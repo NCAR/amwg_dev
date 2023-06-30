3827a13f24cb0be1df4c602f0f5b1e67fb92f296
Cecile Hannay <hannay@ucar.edu>
2023-06-29 20:25:41 -0600

    case files for f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi1.0.001

--- replay.sh
+++ replay.sh
@@ -2,25 +2,35 @@
 
 set -e
 
-# Created 2023-06-09 17:30:47
+# Created 2023-06-20 13:44:14
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLT1850.ne30.landspinup.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi1.0.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset 1850_CAM%DEV%LT%GHGMAM4_CLM51%BGC-CROP_CICE%PRES_DOCN%DOM_MOSART_SGLC_SWAV_SESP --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset FLTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
+./xmlchange --append CAM_CONFIG_OPTS=-cosp
+
 ./case.setup
 
-./case.build
+./xmlchange RUN_STARTDATE=1995-01-01
 
-./case.build
+./xmlchange STOP_N=2
 
-./case.build
+./xmlchange STOP_OPTION=nyears
 
-./case.build
+./xmlchange RESUBMIT=5
 
-./case.build
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+
+./xmlchange RUN_REFDATE=1994-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
 
 ./case.build
 
@@ -29,30 +39,4 @@ cd "${CASEDIR}"
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./case.submit
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.build --clean-all
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=15
 
