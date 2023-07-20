d9d530c0b48cfbaaf2f20e54403e550f9b895b41
Cecile Hannay <hannay@ucar.edu>
2023-07-20 16:13:23 -0600

    case files for f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi2.0-wsubi3.5-newhb.001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/322
+Issue https://github.com/NCAR/amwg_dev/issues/323
 
 
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-06-30 12:24:15
+# Created 2023-07-06 10:33:20
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5_zm_water.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_112.FLTHIST_v0c.ne30.non-ogw-ubcT-effgw0.3-rdg_beta0.5-vtrmi2.0-wsubi3.5-newhb.001"
 
 /glade/work/hannay/cesm_tags/cam6_3_112/cime/scripts/create_newcase --compset FLTHIST_v0c --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -36,17 +36,9 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
 ./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
-
-./case.submit
-
-./case.submit
-
-./case.submit
 
 ./case.submit
 
