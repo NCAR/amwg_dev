142eca7086a2d4cfbdbaef900369467ca469b74a
Cecile Hannay <hannay@ucar.edu>
2023-07-20 16:36:29 -0600

    case files for f.cam6_3_117.FLTHIST_ne30.r250.wsub.001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/328
+Issue https://github.com/NCAR/amwg_dev/issues/330
 
 
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-07-07 17:06:37
+# Created 2023-07-14 13:17:51
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_117.FLTHIST_ne30.r250_gamma_coefb0.3.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_117.FLTHIST_ne30.r250.wsub.001"
 
 /glade/work/hannay/cesm_tags/cam6_3_117/cime/scripts/create_newcase --compset FLTHIST_v0d --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
@@ -34,45 +34,15 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=10,STOP_N=1,STOP_OPTION=nyears
+./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=25,STOP_N=3,STOP_OPTION=nmonthss
 
 ./xmlchange REST_OPTION=nmonths,REST_N=3
 
 ./case.submit
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=25,STOP_N=3,STOP_OPTION=nmonths
+./xmlchange STOP_OPTION=nmonths
 
-./xmlchange PROJECT=P93300642,JOB_QUEUE=regular,RESUBMIT=25,STOP_N=3,STOP_OPTION=nmonths
-
-./xmlchange REST_OPTION=nmonths,REST_N=3
-
-./xmlchange CONTINUE_RUN=TRUE
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=20
-
-./case.submit
+./xmlchange STOP_OPTION=nmonths,CONTINUE_RUN=TRUE
 
 ./case.submit
 
