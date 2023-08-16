5ebb7a695d25fae75711b1c6dd20a04da4679b1c
Cecile Hannay <hannay@ucar.edu>
2023-08-16 16:27:40 -0600

    case files for f.cam6_3_119.FMTHIST_ne30.r328.001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/346
+Issue https://github.com/NCAR/amwg_dev/issues/347
 
 
 
--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2023-07-21 19:54:02
+# Created 2023-07-22 16:20:32
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_119.FLTHIST_ne30.r328-wsub_scale0.5.001"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_119.FMTHIST_ne30.r328.001"
 
-/glade/work/hannay/cesm_tags/cam6_3_119/cime/scripts/create_newcase --compset FLTHIST_v0d --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_119/cime/scripts/create_newcase --compset FMTHIST_v0d --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
@@ -24,15 +24,39 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_TYPE=hybrid
 
-./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
+./xmlchange RUN_REFCASE=f.cam6_3_110.FMTHIST_v0c.ne30.tuningF.001
 
-./xmlchange RUN_REFDATE=1994-01-01
+./xmlchange RUN_REFDATE=1996-01-01
 
 ./xmlchange GET_REFCASE=TRUE
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
 ./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./case.build
+
+./case.build --clean-all
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./case.submit
 
 ./preview_namelists
 
@@ -43,4 +67,8 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
+
+./case.submit
+
+./xmlchange RESUBMIT=0
 
