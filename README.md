6528187a145bc87a62eb29dee87155f9e913d0fd
Cecile Hannay <hannay@ucar.edu>
2023-08-29 16:41:44 -0600

    case files for b.e23_alpha16b.BLT1850.ne30_t232.039

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/370
+https://github.com/NCAR/amwg_dev/issues/372
 
--- replay.sh
+++ replay.sh
@@ -1,32 +1,22 @@
-#!/bin/bash
-
-set -e
-
-# Created 2023-08-17 16:19:18
-
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16b.BLT1850.ne30_t232.038"
-
-/glade/work/hannay/cesm_tags/cesm2_3_alpha16b/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
-
-cd "${CASEDIR}"
+./case.setup
 
 ./case.setup
 
-./preview_namelists
-
-./xmlchange --append CAM_CONFIG_OPTS=-cosp
-
 ./case.build
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./case.build --clean-all
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./case.build
 
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+./xmlchange RESUBMIT=10
+
+./check_case
 
 ./case.submit
+
+./xmlchange RESUBMIT=19
 
 ./case.submit
 
