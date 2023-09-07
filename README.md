be426f98012f870ec453882d7398e79a34b2bc77
Cecile Hannay <hannay@ucar.edu>
2023-09-07 13:40:32 -0600

    case files for f.cam6_3_119.FLTHIST_ne30.r328_gamma0.33_soae_dst1.3.001

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # amwg_cases
 
-Issue https://github.com/NCAR/amwg_dev/issues/389
+Issue https://github.com/NCAR/amwg_dev/issues/393
 
 
 
--- replay.sh
+++ replay.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# Created 2023-08-31 11:03:22
+# Created 2023-08-31 17:17:45
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.e23_alpha16b.FLT2000.ne30.033_pia"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/f.cam6_3_119.FLTHIST_ne30.r328_gamma0.33_soae_dst1.3.001"
 
-/glade/work/hannay/cesm_tags/cesm2_3_alpha16b/cime/scripts/create_newcase --compset 2000_CAM%DEV%LT%GHGMAM4_CLM51%SP_CICE%PRES_DOCN%DOM_MOSART_SGLC_SWAV_SESP --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
+/glade/work/hannay/cesm_tags/cam6_3_119/cime/scripts/create_newcase --compset FLTHIST_v0d --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --pecount 2160 --project 93300722
 
 cd "${CASEDIR}"
 
@@ -32,23 +32,29 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_REFDIR=cesm2_init
 
-./preview_namelists
+./xmlchange --append CAM_CONFIG_OPTS=-cosp
 
-./preview_namelists
+./case.setup
 
-./case.build
+./xmlchange RUN_STARTDATE=1995-01-01
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=1,STOP_OPTION=nyears
+./xmlchange STOP_N=2
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./xmlchange STOP_OPTION=nyears
 
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+./xmlchange RESUBMIT=5
 
-./case.submit
+./xmlchange RUN_TYPE=hybrid
 
-./case.submit
+./xmlchange RUN_REFCASE=f.cam6_3_107.FLTHIST_v0a.ne30.clm5_1.001
 
-./xmlchange RUN_STARTDATE=0001-01-01
+./xmlchange RUN_REFDATE=1994-01-01
+
+./xmlchange GET_REFCASE=TRUE
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./xmlchange RUN_STARTDATE=1995-01-01
 
 ./xmlchange STOP_N=2
 
@@ -68,11 +74,13 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=1,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=1,STOP_OPTION=nyears
 
