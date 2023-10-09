57f59b10b96d674f4bf15d7f87dbd0e9267827a0
Cecile Hannay <hannay@ucar.edu>
2023-10-09 14:14:07 -0600

    case files for b.e23_alpha16b.BLT1850.ne30_t232.051

--- replay.sh
+++ replay.sh
@@ -2,55 +2,35 @@
 
 set -e
 
-# Created 2023-10-05 16:16:40
+# Created 2023-10-06 14:05:28
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16b.BLT1850.ne30_t232.050"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16b.BLT1850.ne30_t232.051"
 
-/glade/work/gmarques/cesm.sandboxes/cesm2_3_alpha16b_t025/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case b.e23_alpha16b.BLT1850.ne30_t232.050 --run-unsupported --project CESM0023
+/glade/work/hannay/cesm_tags/cesm2_3_alpha16b/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
+./case.setup
+
+./preview_namelists
+
 ./xmlchange --append CAM_CONFIG_OPTS=-cosp
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./case.build
 
 ./xmlchange ROF2OCN_LIQ_RMAPNAME=/glade/work/gmarques/cesm/tx2_3/runoff_mapping/map_r05_to_tx2_3_nnsm_e250r250_230914.nc
 
 ./xmlchange GLC2OCN_LIQ_RMAPNAME=/glade/work/gmarques/cesm/tx2_3/runoff_mapping/map_gland4km_to_tx2_3_nnsm_e250r250_230914.nc
 
-./case.setup
-
 ./case.build
 
-./check_case
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
-./pelayout
-
-./case.submit
-
-./case.submit
-
-./case.setup --reset
-
-./case.build --clean-all
-
-./case.build
-
-./case.submit
+./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
-./case.build
-
 ./case.submit
 
-./case.submit
-
-./case.build
-
-./case.submit
-
-./case.submit
-
-./case.submit
+./xmlchange PROJECT=CESM0023,RESUBMIT=12,STOP_N=2,STOP_OPTION=nyears
 
