1d5684748b673023aca0e25267f0b6a7be8c0eea
Cecile Hannay <hannay@ucar.edu>
2023-12-18 13:02:13 -0700

    case files for b.e23_alpha16g.BLT1850.ne30_t232.064

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-11-29 07:54:52
+# Created 2023-12-08 09:53:32
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.063"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.064"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,7 +18,11 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
+./case.build
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=12,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
@@ -28,17 +32,7 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./xmlchange JOB_QUEUE=5
-
 ./case.submit
-
-./xmlchange RESUBMIT=5
-
-./xmlchange JOB_QUEUE=main
 
 ./xmlchange RESUBMIT=10
-
-./case.submit
-
-./xmlchange RESUBMIT=0
 
