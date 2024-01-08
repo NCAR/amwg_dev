2a2fe8431ebcb3994bd51c529d908b8fd91a03c7
Cecile Hannay <hannay@ucar.edu>
2024-01-08 10:25:11 -0700

    case files for b.e23_alpha16g.BLT1850.ne30_t232.070

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/448
+https://github.com/NCAR/amwg_dev/issues/451
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2023-12-18 11:30:44
+# Created 2023-12-19 14:07:35
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.069"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.070"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,15 +18,13 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=12,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=4
 
