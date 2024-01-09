283f0694af662754f925af553312ea50751082f4
Cecile Hannay <hannay@ucar.edu>
2024-01-09 11:44:15 -0700

    case files for b.e23_alpha16g.BLT1850.ne30_t232.075

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 # case: 
-https://github.com/NCAR/amwg_dev/issues/461
+https://github.com/NCAR/amwg_dev/issues/462
+
 
 
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-01-05 14:00:36
+# Created 2024-01-08 15:15:17
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.074"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.075"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -13,32 +13,6 @@ cd "${CASEDIR}"
 ./case.setup
 
 ./preview_namelists
-
-./case.setup
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=5,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00
-
-./case.build
-
-./case.build
 
 ./case.build
 
@@ -49,6 +23,4 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
-
-./xmlchange RESUBMIT=0
 
