c97ef3d2ec274cda857c736302751b1ae0bb20e8
Cecile Hannay <hannay@ucar.edu>
2024-01-31 15:08:29 -0700

    case files for b.e23_alpha16g.BLT1850.ne30_t232.078

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-01-13 17:56:03
+# Created 2024-01-26 13:55:46
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.076"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.078"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -14,9 +14,21 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./case.setup
-
 ./preview_namelists
+
+./case.build
+
+./case.setup --reset
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.build --clean-all
 
 ./case.build
 
@@ -27,16 +39,4 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
-
-./case.submit
-
-./case.submit
-
-./case.submit
-
-./xmlchange RESUBMIT=10
-
-./case.submit
-
-./xmlchange RESUBMIT=20
 
