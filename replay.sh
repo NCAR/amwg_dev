0d928543cf6ea89fe9da479e174936cac9d55838
Cecile Hannay <hannay@ucar.edu>
2024-02-08 13:46:45 -0700

    case files for b.e23_alpha16g.BLT1850.ne30_t232.079

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-01-26 13:55:46
+# Created 2024-01-31 11:18:42
 
-CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.078"
+CASEDIR="/glade/p/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.079"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,7 +18,17 @@ cd "${CASEDIR}"
 
 ./case.build
 
-./case.setup --reset
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./case.build
 
 ./case.build
 
@@ -27,6 +37,12 @@ cd "${CASEDIR}"
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./xmlchange NTASKS_OCN=512
+
+./case.setup --reset
 
 ./case.build --clean-all
 
@@ -37,6 +53,8 @@ cd "${CASEDIR}"
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
 
 ./case.submit
 
