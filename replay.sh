e07df7636b0fe229b4f242a7e81a0abaf3114715
Cecile Hannay <hannay@ucar.edu>
2024-04-17 11:49:15 -0600

    case files for b.e23_alpha16g.BLT1850.ne30_t232.088b

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-03-15 15:41:29
+# Created 2024-04-15 14:06:55
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.083b"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232.088b"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -26,21 +26,13 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
 ./case.build
 
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=15,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
-
-./xmlchange RESUBMIT=0
 
