170e33f9a808b5189335ba745572da547b5a9970
Cecile Hannay <hannay@ucar.edu>
2024-08-13 13:55:25 -0600

    case files for b.e23_alpha17f.BLT1850.ne30_t232.102

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: b.e23_alpha17f.BLT1850.ne30_t232.101
-https://github.com/NCAR/amwg_dev/issues/563
+# case: b.e23_alpha17f.BLT1850.ne30_t232.102
+https://github.com/NCAR/amwg_dev/issues/564
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-07-26 10:33:44
+# Created 2024-07-26 10:51:08
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_t232.101"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_t232.102"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha17f/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -36,10 +36,6 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
-./preview_namelists
-
 ./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange CHARGE_ACCOUNT=CESM0023
@@ -47,10 +43,6 @@ cd "${CASEDIR}"
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./case.submit
-
-./case.submit
 
 ./case.submit
 
