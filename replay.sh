9d11b57074cde2535333c1b3e1e00c6c9f47bb6a
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-09-09 16:05:07 -0600

    case files for b.e23_alpha16g.BLT1850.ne30_t232_L32.082b

--- replay.sh
+++ replay.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-# Created 2024-04-24 14:45:10
+# Created 2024-04-19 12:03:08
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232_L42.082b"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232_L32.082b"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
-
-./xmlchange CAM_CONFIG_OPTS="-phys cam_dev -chem ghg_mam4 -nlev 42 -model_top lt"
 
 ./case.setup
 
@@ -28,47 +26,41 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
 ./case.build
 
-./xmlchange RUN_REFCASE=b.e23_alpha16g.BLT1850.ne30_t232.075
-
-./xmlchange RUN_REFDATE=0101-01-01
-
-./xmlchange RUN_TYPE=hybrid
-
-./xmlchange GET_REFCASE=true
-
-./xmlchange RUN_REFDIR=cesm2_init
-
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./xmlchange PROJECT=CESM0023,RESUBMIT=24,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./preview_namelists
+
+./case.build
+
+./preview_namelists
+
+./case.build
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=24,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
-./xmlchange RUN_REFCASE=b.e23_alpha16g.BLT1850.ne30_t232.075
+./case.submit
 
-./xmlchange RUN_REFDATE=0101-01-01
+./xmlchange CAM_CONFIG_OPTS="-phys cam_dev -chem ghg_mam4 -nlev 58 -model_top lt"
 
-./xmlchange RUN_TYPE=hybrid
+./xmlchange CAM_CONFIG_OPTS="-phys cam_dev -chem ghg_mam4 -nlev 32 -model_top lt"
 
-./xmlchange GET_REFCASE=true
+./case.build --clean-all
 
-./xmlchange RUN_REFDIR=cesm2_init
+./xmlchange CAM_CONFIG_OPTS="-phys cam_dev -chem ghg_mam4 -nlev 32 -model_top lt"
 
 ./preview_namelists
+
+./case.build
 
 ./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
