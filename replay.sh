ab68c74115498dc1d296798a62255f06f5a875da
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-09-09 15:54:49 -0600

    case files for b.e23_alpha17f.BLT1850.ne30_t232.095

--- replay.sh
+++ replay.sh
@@ -2,51 +2,45 @@
 
 set -e
 
-# Created 2024-05-30 16:19:31
+# Created 2024-05-28 17:16:40
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_g17.POP.096"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_t232.095"
 
-/glade/work/hannay/cesm_tags/cesm2_3_alpha17f/cime/scripts/create_newcase --compset 1850_CAM%DEV%LT%GHGMAM4_CLM51%BGC-CROP_CICE_POP2%ECO_MOSART_CISM2%GRIS-NOEVOLVE_WW3_SESP --res ne30pg3_g17 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cesm2_3_alpha17f/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
+
+./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
 
 ./case.setup
 
 ./preview_namelists
 
-./xmlchange --noecho CPL_I2O_PER_CAT=TRUE
+./xmlchange MOM6_VERTICAL_GRID=hycom1
 
 ./preview_namelists
 
 ./preview_namelists
-
-./case.build
 
 ./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange CHARGE_ACCOUNT=CESM0023
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
 ./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
 
-./case.submit
-
-./case.setup --reset
-
-./case.build --clean
-
-./preview_namelists
-
-./case.build
-
 ./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange CHARGE_ACCOUNT=CESM0023
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./case.build
 
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+./case.build
+
+./case.build
+
+./case.build
+
+./case.submit
 
 ./case.submit
 
@@ -54,7 +48,13 @@ cd "${CASEDIR}"
 
 ./case.submit
 
+./xmlchange RESUBMIT=20
+
 ./case.submit
+
+./case.submit
+
+./case.build
 
 ./case.submit
 
