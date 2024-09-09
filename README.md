e17278b62e14ca9efd70c547d58e7b0990088d67
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-09-09 16:01:11 -0600

    case files for b.e23_alpha17f.BLT1850.ne30_t232.093

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: b.e23_alpha17f.BLT1850.ne30_t232.094
-https://github.com/NCAR/amwg_dev/issues/539
+# case: b.e23_alpha17f.BLT1850.ne30_t232.093
+https://github.com/NCAR/amwg_dev/issues/538
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-05-24 18:14:11
+# Created 2024-05-23 14:37:23
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_t232.094"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha17f.BLT1850.ne30_t232.093"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha17f/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,47 +18,59 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
+./xmlchange MOM6_VERTICAL_GRID=hycom1
+
+./preview_namelists
+
 ./case.build
 
-./xmlchange CHARGE_ACCOUNT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=6:00:00
-
-./xmlchange CHARGE_ACCOUNT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=12:00:00
-
-./xmlchange CHARGE_ACCOUNT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=1
-
-./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=12:00:00
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+./preview_namelists
 
 ./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
 
 ./xmlchange REST_OPTION=nyears,REST_N=1
 
-./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=12:00:00
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./case.submit
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange CHARGE_ACCOUNT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange CHARGE_ACCOUNT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=6:00:00
 
 ./case.submit
 
 ./case.submit
 
-./xmlchange RESUBMIT=18
+./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
 
-./case.submit
+./preview_namelists
 
-./case.submit
+./case.build
 
-./case.submit
+./case.build --clean-all
 
-./case.submit
+./preview_namelists
+
+./case.build
 
 ./case.submit
 
