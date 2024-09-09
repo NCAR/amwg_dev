9e2cebd07efea7ad888c8fecbd5f1535790d4855
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-09-09 16:04:32 -0600

    case files for b.e23_alpha16g.BLT1850.ne30_t232_L42.082b

--- replay.sh
+++ replay.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-# Created 2024-04-24 21:15:59
+# Created 2024-04-24 14:45:10
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232_daily.082b"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/b.e23_alpha16g.BLT1850.ne30_t232_L42.082b"
 
 /glade/work/hannay/cesm_tags/cesm2_3_alpha16g/cime/scripts/create_newcase --compset BLT1850_v0c --res ne30pg3_t232 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
+
+./xmlchange CAM_CONFIG_OPTS="-phys cam_dev -chem ghg_mam4 -nlev 42 -model_top lt"
 
 ./case.setup
 
@@ -26,19 +28,53 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
+./preview_namelists
+
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=1,STOP_N=1,STOP_OPTION=nyears
+./xmlchange RUN_REFCASE=b.e23_alpha16g.BLT1850.ne30_t232.075
 
-./xmlchange REST_OPTION=nyears,REST_N=1
+./xmlchange RUN_REFDATE=0101-01-01
 
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
 
 ./preview_namelists
 
 ./case.build
 
-./case.build
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
+
+./xmlchange RUN_REFCASE=b.e23_alpha16g.BLT1850.ne30_t232.075
+
+./xmlchange RUN_REFDATE=0101-01-01
+
+./xmlchange RUN_TYPE=hybrid
+
+./xmlchange GET_REFCASE=true
+
+./xmlchange RUN_REFDIR=cesm2_init
+
+./preview_namelists
+
+./xmlchange PROJECT=CESM0023,RESUBMIT=10,STOP_N=2,STOP_OPTION=nyears
+
+./xmlchange REST_OPTION=nyears,REST_N=1
+
+./xmlchange JOB_WALLCLOCK_TIME=12:00:00
 
 ./case.submit
 
