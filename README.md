7113cab4817b4aa33b48749cb32af515568b43f1
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-11-06 13:25:05 -0700

    case files for f.e30_alpha04a.FMTHIST.ne30_L93.002

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_alpha04a.FMTHIST.ne30_L93.001
-https://github.com/NCAR/amwg_dev/issues/604
+# case: f.e30_alpha04a.FMTHIST.ne30_L93.002
+https://github.com/NCAR/amwg_dev/issues/605
--- replay.sh
+++ replay.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-# Created 2024-10-26 10:18:31
+# Created 2024-10-29 15:10:01
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_alpha04a.FMTHIST.ne30_L93.001"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_alpha04a.FMTHIST.ne30_L93.002"
 
 /glade/work/hannay/cesm_tags/cesm3_0_alpha04a/cime/scripts/create_newcase --compset FMTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
-
-./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
 
 ./xmlchange NTASKS=2304
 
@@ -22,21 +20,33 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
+./preview_namelists
+
+./preview_namelists
+
 ./case.build
 
-./xmlchange PROJECT=CESM0023,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
+./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
 
-./xmlchange CHARGE_ACCOUNT=CESM0023,PROJECT=CESM0023
+./case.setup --reset
 
-./xmlchange REST_OPTION=nyears,REST_N=2
+./preview_namelists
 
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
+./case.build
 
-./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
+./case.setup --reset
 
-./xmlchange JOB_PRIORITY=premium
+./case.build --clean
 
-./case.submit
+./preview_namelists
+
+./preview_namelists
+
+./case.build
+
+./preview_namelists
+
+./preview_namelists
 
 ./xmlchange PROJECT=CESM0023,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
 
