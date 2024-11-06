327503ea902b46feafb163ad2af71ac9c1c9a13e
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-11-06 12:54:22 -0700

    case files for f.e30_cam6_4_036.FMTHIST.ne30_L93.001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_cam6_4_036.FLTHIST.ne30_L58.SST4K.001
-https://github.com/NCAR/amwg_dev/issues/599
+# case: f.e30_cam6_4_036.FMTHIST.ne30_L93.001
+https://github.com/NCAR/amwg_dev/issues/600
--- replay.sh
+++ replay.sh
@@ -2,24 +2,22 @@
 
 set -e
 
-# Created 2024-10-16 10:37:29
+# Created 2024-10-16 14:06:47
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_cam6_4_036.FLTHIST.ne30_L58.SST4K.001"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_cam6_4_036.FMTHIST.ne30_L93.001"
 
-/glade/work/hannay/cesm_tags/cam6_4_036/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
+/glade/work/hannay/cesm_tags/cam6_4_036/cime/scripts/create_newcase --compset FMTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
 ./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
 
-./xmlchange NTASKS=2160
+./xmlchange NTASKS=2304
 
 ./case.setup
 
 ./xmlchange RUN_STARTDATE=2000-01-01
 
-./xmlchange SSTICE_DATA_FILENAME=/glade/campaign/cesm/cesmdata/cseg/inputdata/atm/cam/sst/sst_HadOIBl_bc_1x1_1850_2021_SST4K_c241009.nc
-
 ./preview_namelists
 
 ./preview_namelists
@@ -40,25 +38,9 @@ cd "${CASEDIR}"
 
 ./case.submit
 
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange CHARGE_ACCOUNT=CESM0023,PROJECT=CESM0023
-
-./xmlchange REST_OPTION=nyears,REST_N=2
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
-
-./xmlchange JOB_PRIORITY=premium
-
 ./case.submit
 
-./case.submit
+./xmlchange JOB_PRIORITY=regular
 
 ./xmlchange JOB_PRIORITY=regular
 
