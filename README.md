29c051bf56c4ada5aa6d2642f6d374b12374d217
Cecile Hannay <cecilehannay@users.noreply.github.com>
2024-11-06 21:05:15 -0700

    case files for f.e30_cam6_4_036.FLTHIST.ne30_L32.cam5.rpen.003

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_cam6_4_036.FLTHIST.ne30_L32.cam5.002
-https://github.com/NCAR/amwg_dev/issues/609
+# case: f.e30_cam6_4_036.FLTHIST.ne30_L32.cam5.rpen.003
+https://github.com/NCAR/amwg_dev/issues/610
--- replay.sh
+++ replay.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-# Created 2024-10-22 10:21:59
+# Created 2024-11-06 14:17:53
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_cam6_4_036.FLTHIST.ne30_L32.cam5.002"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_cam6_4_036.FLTHIST.ne30_L32.cam5.rpen.003"
 
 /glade/work/hannay/cesm_tags/cam6_4_036/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
 cd "${CASEDIR}"
 
-./xmlchange CAM_CONFIG_OPTS="-phys cam7 -nlev 32 -model_top lt -rad rrtmgp"
-
 ./xmlchange CAM_CONFIG_OPTS="-phys cam5 -nlev 32"
 
 ./xmlchange NTASKS=1280
@@ -21,44 +19,6 @@ cd "${CASEDIR}"
 ./xmlchange RUN_STARTDATE=2000-01-01
 
 ./preview_namelists
-
-./case.build
-
-./case.submit
-
-././xmlchange PROJECT=CESM0023,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange CHARGE_ACCOUNT=CESM0023,PROJECT=CESM0023
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
-
-./xmlchange JOB_PRIORITY=regular
-
-./case.submit
-
-./xmlchange CAM_CONFIG_OPTS="-phys cam5 -nlev 32"
-
-./xmlchange NTASKS=1280
-
-./case.setup
-
-./xmlchange RUN_STARTDATE=2000-01-01
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
 
 ./preview_namelists
 
@@ -72,7 +32,7 @@ cd "${CASEDIR}"
 
 ./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
 
-./xmlchange JOB_PRIORITY=premium
+./xmlchange JOB_PRIORITY=regular
 
 ./case.submit
 
