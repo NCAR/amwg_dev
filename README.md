a9d42412c385da63f8f4fc886aed6eefed3df83f
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-02-04 09:26:32 -0700

    case files for f.e30_beta04.FLTHIST.ne30.lwp2.2.001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_beta04.FLTHIST.ne30.001
-https://github.com/NCAR/amwg_dev/issues/621
+# case: f.e30_beta04.FLTHIST.ne30.lwp2.2.001
+https://github.com/NCAR/amwg_dev/issues/622
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-12-18 14:26:54
+# Created 2024-12-18 18:41:32
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.001"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.lwp2.2.001"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -19,32 +19,6 @@ cd "${CASEDIR}"
 ./case.setup
 
 ./xmlchange RUN_STARTDATE=2000-01-01
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./preview_namelists
-
-./case.build
 
 ./preview_namelists
 
@@ -79,14 +53,6 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
 
 ./xmlchange JOB_PRIORITY=regular
-
-./case.submit
-
-./case.submit
-
-./preview_namelists
-
-./case.build
 
 ./case.submit
 
