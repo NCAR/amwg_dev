9ac293c1116177c93978c177ee9532a7216a99fb
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-02-04 09:59:45 -0700

    case files for f.e30_beta04.FLTHIST.ne30.convproc_do_aerF.001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_beta04.FLTHIST.ne30.rhminis0.8.001
-https://github.com/NCAR/amwg_dev/issues/637
+# case: f.e30_beta04.FLTHIST.ne30.convproc_do_aerF.001
+https://github.com/NCAR/amwg_dev/issues/638
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-01-19 21:12:04
+# Created 2025-01-19 21:25:42
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.rhminis0.8.001"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.convproc_do_aerF.001"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
