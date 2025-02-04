fad079cb50101d9c571f7e12871db32d15518cf4
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-02-04 09:14:59 -0700

    case files for f.e30_alpha04a.FMTHIST.ne30_L93_tuning.008

--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2024-11-08 13:51:57
+# Created 2024-11-08 16:03:12
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_alpha04a.FMTHIST.ne30_L93_tuning.007"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_alpha04a.FMTHIST.ne30_L93_tuning.008"
 
 /glade/work/hannay/cesm_tags/cesm3_0_alpha04a/cime/scripts/create_newcase --compset FMTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -18,35 +18,11 @@ cd "${CASEDIR}"
 
 ./xmlchange RUN_STARTDATE=2000-01-01
 
-./xmlchange --append CAM_CONFIG_OPTS="-rad rrtmgp"
-
-./xmlchange NTASKS=2304
-
-./case.setup
-
-./xmlchange RUN_STARTDATE=2000-01-01
-
 ./preview_namelists
 
 ./preview_namelists
 
 ./case.build
-
-./preview_namelists
-
-./case.build
-
-./xmlchange PROJECT=CESM0023b,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
-
-./xmlchange REST_OPTION=nyears,REST_N=2
-
-./xmlchange JOB_WALLCLOCK_TIME=12:00:00 --subgroup case.run
-
-./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
-
-./xmlchange JOB_PRIORITY=regular
-
-./case.submit
 
 ./xmlchange PROJECT=P93300642,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
 
@@ -57,6 +33,8 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
 
 ./xmlchange JOB_PRIORITY=regular
+
+./case.submit
 
 ./case.submit
 
