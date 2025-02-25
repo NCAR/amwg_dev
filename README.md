494f5fafb16f928c67a4fc3d6a3a0eab1b62dd29
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-02-25 11:54:52 -0700

    case files for f.e30_beta04.FLTHIST.ne30.dp1_0.05.001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_beta04.FLTHIST.ne30.001_pia
-https://github.com/NCAR/amwg_dev/issues/641
+# case: f.e30_beta04.FLTHIST.ne30.dp1_0.05.001
+https://github.com/NCAR/amwg_dev/issues/642
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-01-31 15:03:55
+# Created 2025-02-24 15:57:40
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.001_pia"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.dp1_0.05.001"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
@@ -22,15 +22,9 @@ cd "${CASEDIR}"
 
 ./preview_namelists
 
-./preview_namelists
-
 ./case.build
 
 ./preview_namelists
-
-./preview_namelists
-
-./case.build
 
 ./xmlchange PROJECT=CESM0023,RESUBMIT=4,STOP_N=2,STOP_OPTION=nyears
 
@@ -43,22 +37,6 @@ cd "${CASEDIR}"
 ./xmlchange JOB_WALLCLOCK_TIME=06:00:00 --subgroup case.st_archive
 
 ./xmlchange JOB_PRIORITY=regular
-
-./case.submit
-
-./case.build
-
-./case.submit
-
-./case.build
-
-./case.build
-
-./case.build --clean
-
-./preview_namelists
-
-./case.build
 
 ./case.submit
 
