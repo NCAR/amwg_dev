1013974a2b389794147835ba2515332c19c3cd74
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-02-25 12:00:15 -0700

    case files for f.e30_beta04.FLTHIST.ne30.ke_lnd_5.001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# case: f.e30_beta04.FLTHIST.ne30.ke_2.5.001
-https://github.com/NCAR/amwg_dev/issues/644
+# case: f.e30_beta04.FLTHIST.ne30.ke_lnd_5.001
+https://github.com/NCAR/amwg_dev/issues/645
--- replay.sh
+++ replay.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Created 2025-02-25 10:30:15
+# Created 2025-02-25 10:52:59
 
-CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.ke_2.5.001"
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_beta04.FLTHIST.ne30.ke_lnd_5.001"
 
 /glade/work/hannay/cesm_tags/cesm3_0_beta04/cime/scripts/create_newcase --compset FLTHIST --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
 
