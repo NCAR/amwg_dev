f6b194e416cd6753b8bf9a5aad4fe5048ef72df6
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-06-20 15:36:08 -0600

    local git repository created

---
+++ replay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Created 2025-06-20 15:36:04
+
+CASEDIR="/glade/campaign/cesm/cesmdata/cseg/runs/cesm2_0/f.e30_cam6_4_097.FHISTMT_LTso.ne30_setting166.001"
+
+/glade/work/hannay/cesm_tags/cam6_4_097/cime/scripts/create_newcase --compset FHISTC_MTso --res ne30pg3_ne30pg3_mg17 --case "${CASEDIR}" --run-unsupported --project 93300722
+
+cd "${CASEDIR}"
+
+./xmlchange --append CAM_CONFIG_OPTS=-cosp
+
+./xmlchange NTASKS=2160
+
+./case.setup
+
