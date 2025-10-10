09d939b2d9347b45a2f73295cb6ed20151302924
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-10-10 17:36:00 -0600

    <command> ./xmlchange SSTICE_DATA_FILENAME=/glade/derecho/scratch/hannay/sst_ne120/sst_input_0.25_CDR-19900101-19901231.nc  </command>

--- replay.sh
+++ replay.sh
@@ -120,3 +120,5 @@ cd "${CASEDIR}"
 
 ./xmlchange SSTICE_DATA_FILENAME=/glade/work/xueliu/data/cesm_input/SST/SSTinput_CDR_noleap/sst_input_0.25_CDR-19900101-19901231.nc
 
+./xmlchange SSTICE_DATA_FILENAME=/glade/derecho/scratch/hannay/sst_ne120/sst_input_0.25_CDR-19900101-19901231.nc
+
