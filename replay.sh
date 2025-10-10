f4e1855b1062a88ded736cd0cbc11ef6a7492821
Cecile Hannay <cecilehannay@users.noreply.github.com>
2025-10-10 16:04:31 -0600

    <command> ./xmlchange SSTICE_MESH_FILENAME=/glade/campaign/cgd/amp/juliob/NOAA_OI_SST/sst_ice_NOAA_QxQ_ESMFmesh.nc  </command>

--- replay.sh
+++ replay.sh
@@ -34,3 +34,5 @@ cd "${CASEDIR}"
 
 ./xmlchange SSTICE_DATA_FILENAME=/glade/work/xueliu/data/cesm_input/SST/SSTinput_CDR_noleap/sst_input_0.25_CDR-19900101-19901231.nc
 
+./xmlchange SSTICE_MESH_FILENAME=/glade/campaign/cgd/amp/juliob/NOAA_OI_SST/sst_ice_NOAA_QxQ_ESMFmesh.nc
+
